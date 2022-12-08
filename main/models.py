300430266010a3d73f77020d827914f44fcb175d
Abdulhamid <zuhriddinovabdulhamid@gmail.com>
2022-12-08 10:51:25 +0500

    models, apps created
    main asosiy client bilan ishlaydi, attendance davomat uchun, payment to'lovlar uchun app sifatida qo'shib qo'ydim. ishni bo'lib olganimiz yaxshi

---
+++ main/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Client(models.Model):
+    user = models.ForeignKey(User, related_name="clients", on_delete=models.CASCADE)
+
+    name = models.CharField("Ism familiyasi", max_length=150)
+    phone = models.CharField("Telefon raqami", max_length=50)
+    coming_type = models.ForeignKey("main.ComingType", related_name="type_clients",
+                                    on_delete=models.SET_NULL, blank=True, null=True)
+    debt = models.BooleanField("Qarzi bor", default=False)
+    created = models.DateField("Qo'shilgan sana", auto_now_add=True)
+    updated = models.DateTimeField("O'zgartirilgan vaqt", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ComingType(models.Model):
+    title = models.CharField("Tarif nomi", max_length=50)
+    days = models.PositiveIntegerField("Kunlari(bir oyda nech kun)")
+    price = models.PositiveIntegerField("Narxi")
+    info = models.TextField("Tarif xaqida ma'lumot", blank=True)
+
+    def __str__(self):
+        return self.title
