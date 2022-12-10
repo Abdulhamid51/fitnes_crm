c8b648469b55e0a6bbc1446e7fb3f0956dbc7a69
Abdulhamid <zuhriddinovabdulhamid@gmail.com>
2022-12-10 11:52:19 +0500

    Update models.py
    heyingi vazifa sahifalarni hammasini ulash

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+    month = models.ForeignKey("appendance.Month", related_name="payments", on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    money = models.IntegerField("Puli")
+
+    def __str__(self):
+        return str(self.date)
