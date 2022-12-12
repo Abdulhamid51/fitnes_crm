eb00df97458f7b554ee556f7bcb9fe93657132c1
Web Program <webprogram@Server.set>
2022-12-12 10:29:55 +0500

    status models.py

--- main/models.py
+++ main/models.py
@@ -8,6 +8,12 @@ class Client(models.Model):
     phone = models.CharField("Telefon raqami", max_length=50)
     coming_type = models.ForeignKey("main.ComingType", related_name="type_clients",
                                     on_delete=models.SET_NULL, blank=True, null=True)
+    STATUS_TYPES = (
+        ("ACTIVE","active"),
+        ("INACTIVE","inactive"),
+        ("PAUSED","paused")
+    )
+    status = models.CharField(choices=STATUS_TYPES, max_length=50, blank=True, null=True)
     debt = models.BooleanField("Qarzi bor", default=False)
     created = models.DateField("Qo'shilgan sana", auto_now_add=True)
     updated = models.DateTimeField("O'zgartirilgan vaqt", auto_now=True)
