799c2efe723b2b4609edbd49d39f8da6fa105ca2
abdulatipovO <oyatillohacker11@gmail.com>
2022-12-12 23:20:15 +0500

    index.html and views.py edited

--- main/views.py
+++ main/views.py
@@ -5,8 +5,30 @@ from .models import *
 
 class HomeView(View):
     def get(self,request):
+        clients = Client.objects.all()
+        all_clients = clients.count()
+        # active = Client.objects.filter(status="ACTIVE").count()   ## variant 1.
+        # inactive = Client.objects.filter(status="INACTIVE").count()
+        # paused = Client.objects.filter(status="PAUSED").count()
+
+        inactive = 0
+        active = 0
+        paused = 0
+        for c in clients:
+            if c.status == "ACTIVE":
+                active += 1
+            elif c.status == "INACTIVE":          ## variant 2..
+                inactive += 1
+            else:
+                paused += 1
         
-        return render (request,'index.html')
+        context = {
+            "all_clients":all_clients,
+            "active":active,
+            "inactive":inactive,
+            "paused":paused
+        }
+        return render (request,'index.html',context)
     
 
 class RegisterView(View):
