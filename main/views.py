70824cbff9883b1a95322fd383c261089c5a7d83
Web Program <webprogram@Server.set>
2022-12-12 11:53:33 +0500

    data table

--- main/views.py
+++ main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.views import View
+from .models import *
 
 
 class HomeView(View):
     def get(self,request):
+        
         return render (request,'index.html')
     
 
