e38df55ea4fc9783c954e918d6c20a83fd53c8d8
Abdulhamid <zuhriddinovabdulhamid@gmail.com>
2022-12-21 10:59:03 +0500

    default add day and month

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from . views import DavomatView , StaticView
+from . views import *
 
 app_name = 'attendance'
 
 urlpatterns = [
     path ('',DavomatView.as_view(),name='list_client'),
     path ('static',StaticView.as_view(),name='static'),
+    path ('add_default_day/<int:client_id>', default_add_day, name='add_day'),
+    path ('add_default_month/<int:client_id>', default_add_month, name='add_month'),
 ]
